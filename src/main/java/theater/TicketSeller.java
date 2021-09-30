290858bac01e4f31a995b3dadf15a290e20f7e4f
wilgur513 <wilgur513@naver.com>
2021-09-30 22:04:08 +0900

    extract TicketSeller.sellTo()

--- src/main/java/theater/TicketSeller.java
+++ src/main/java/theater/TicketSeller.java
@@ -7,7 +7,19 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
+    public void sellTo(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+
+    private TicketOffice getTicketOffice() {
         return ticketOffice;
     }
 
